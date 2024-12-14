5fce3a275f4ff66c6ed7e17e31a498890e0653f3
prashantrajput11 <prashanttanwar.work@gmail.com>
2024-12-14 22:26:48 +0530

    added react navigation bottom tabs

--- App.tsx
+++ App.tsx
@@ -1,14 +1,65 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
 import Home from "./src/screens/Home";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import {
+	BottomTabBar,
+	createBottomTabNavigator,
+} from "@react-navigation/bottom-tabs";
+import Discover from "./src/screens/Discover";
+import Profile from "./src/screens/Profile";
+import { Colors } from "./constants/Colors";
+import Ionicons from "@expo/vector-icons/Ionicons";
 
+const Stack = createNativeStackNavigator();
+
+const BottomTabs = createBottomTabNavigator();
+
+const MyBottomTabs = () => {
+	return (
+		<BottomTabs.Navigator
+			screenOptions={{
+				headerTintColor: Colors.primary,
+				tabBarActiveTintColor: "red",
+			}}
+		>
+			<BottomTabs.Screen
+				name="Discover"
+				component={Discover}
+				options={{
+					title: "Discover",
+
+					tabBarIcon: ({ color, size }) => (
+						<Ionicons name="earth" size={size} color={Colors.primary} />
+					),
+				}}
+			/>
+			<BottomTabs.Screen
+				name="Profile"
+				component={Profile}
+				options={{
+					title: "Profile",
+					tabBarIcon: ({ color, size }) => (
+						<Ionicons name="people" size={size} color={Colors.primary} />
+					),
+				}}
+			/>
+		</BottomTabs.Navigator>
+	);
+};
 const App = () => {
 	return (
-		<View style={styles.container}>
-			<Text style={styles.text}>Home Screen</Text>
-
-			<Home />
-		</View>
+		<>
+			<NavigationContainer>
+				<Stack.Navigator
+					screenOptions={{ headerShown: false, headerTintColor: "red" }}
+				>
+					<Stack.Screen name="MyBottomTabs" component={MyBottomTabs} />
+					<Stack.Screen name="Home" component={Home} />
+				</Stack.Navigator>
+			</NavigationContainer>
+		</>
 	);
 };
 
